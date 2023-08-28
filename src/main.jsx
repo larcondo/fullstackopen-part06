8a875cd7cd6677d91d8c107e942058de91eed057
larcondo <larcondo90@gmail.com>
2023-08-28 16:29:45 -0300

    change to noteApp with redux and testing libraries

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import noteReducer from './reducers/noteReducer'
 
 import { createStore } from 'redux'
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
+const store = createStore(noteReducer)
 
-const store = createStore(counterReducer)
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
 
 const App = () => {
   return(
     <div>
-      <div>
-        { store.getState() }
-      </div>
-      <button onClick={e => store.dispatch({ type: 'INCREMENT' })}>plus</button>
-      <button onClick={e => store.dispatch({ type: 'DECREMENT' })}>minus</button>
-      <button onClick={e => store.dispatch({ type: 'ZERO' })}>zero</button>
+      <ul>
+        { store.getState().map( note => 
+          <li key={note.id}>
+            Nota { note.content } <strong>{ note.important ? 'important' : '' }</strong>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
